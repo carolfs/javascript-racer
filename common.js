a988620dc9bef1b4c6ddcdd3b69093f0b1330e26
Carolina Feher da Silva <carolfsu@gmail.com>
2025-07-01 14:23:55 +0100

    Changed two billboard images

--- common.js
+++ common.js
@@ -379,7 +379,7 @@ var SPRITES = {
   COLUMN:                 { x:  995, y:    5, w:  200, h:  315 },
   BILLBOARD01:            { x:  625, y:  375, w:  300, h:  170 },
   BILLBOARD06:            { x:  488, y:  555, w:  298, h:  190 },
-  BILLBOARD05:            { x:    5, y:  897, w:  298, h:  190 },
+  BILLBOARD05:            { x:    5, y:  897, w:  244, h:  190 },
   BILLBOARD07:            { x:  313, y:  897, w:  298, h:  190 },
   BOULDER2:               { x:  621, y:  897, w:  298, h:  140 },
   TREE2:                  { x: 1205, y:    5, w:  282, h:  295 },
